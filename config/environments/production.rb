d04fe18c5780987727881e0ce114e6e036bf9064
Christian Nikolajsen <cn@peytz.dk>
2013-10-10 12:49:21 +0200

    Changed RMagic gem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,8 @@ Ladania::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  
+  config.gem "rmagick", :lib => "RMagick"
 
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
